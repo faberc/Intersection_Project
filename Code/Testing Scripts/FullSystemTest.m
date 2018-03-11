863dd6ff1af2a82bc1217dc09697a69fc3855502
faberc <cfaber@pdx.edu>
2018-03-11 11:11:42 -0700

    made some edits for aesthetics

--- Code/Testing Scripts/FullSystemTest.m
+++ Code/Testing Scripts/FullSystemTest.m
@@ -1,23 +1,23 @@
 clear
 clc
 initializeAll();
+
 global readyNS
 global readyEW
-global TLAseq
-global TLCseq
+
 while true
 t = gtime();
 trafficLights();
-turnLanes();
+carSense = turnLanes();
 crossWalkLights();
 crossWalkButtons();
 lux = ambientLight();
 
 if abs(t - round(t)) < 0.01
-fprintf('Time: %i s.\n', round(t));
-fprintf('CW-NS: %i. CW-EW: %i.\n', [readyNS, readyEW]);
-fprintf('TLA: %d. TLC: %d.\n', [TLAseq, TLCseq]);
-fprintf('Lux: %f\n\n', lux);
+  fprintf('Time: %i s.\n', round(t));
+  fprintf('CW-NS: %i. CW-EW: %i.\n', [readyNS, readyEW]);
+  fprintf('CarTLA: %i. CarTLC: %i.\n', ~carSense(1), ~carSense(2));
+  fprintf('Lux: %f\n\n', lux);
 end
 
 end
