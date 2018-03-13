700b6a10c8d5ef5ae2162f880e43d49e5a878d14
faberc <cfaber@pdx.edu>
2018-03-12 23:08:34 -0700

    edited gtreset

--- Code/gtreset.m
+++ Code/gtreset.m
@@ -1,5 +1,6 @@
-function gtreset()
+function gtreset(s)
 % This function sets or resets a tic timer "gtimer" each time it is called.
+% Inputs: s (speed)
 % The timer is set as a global variable, and can be called with:
 % ''gtreset()'' (starts or resets the timer)
 % ''global gtimer'' (calls the global variable)
@@ -7,7 +8,10 @@ function gtreset()
 % Call the function at the beginning of any 45 second loop.
 
 % Tested and verified by Chuck on 3/1/2018
-
+global speed
 global gtimer
+
+speed = s; % Set global variable speed to s.
+
 gtimer = tic;
 end
